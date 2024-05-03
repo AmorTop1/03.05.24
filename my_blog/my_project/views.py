29783d79ec65f6da5e3ac82edcd5f55145760309
AmorTop1 <evgengrebinec@gmail.com>
2024-05-03 20:35:08 +0300

    first commit

---
+++ my_blog/my_project/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+def captcha_form(request):
+    if request.method == 'POST':
+        captcha_input = request.POST.get('captcha_input')
+        captcha_number = request.POST.get('captcha_number')
+        if captcha_input == captcha_number:
+            return HttpResponse("CAPTCHA введено вірно!")
+        else:
+            return HttpResponse("Неправильний CAPTCHA. Спробуйте ще раз.")
+    else:
+        # Генеруємо випадкове число від 1000 до 9999
+        captcha_number = random.randint(1000, 9999)
+        return render(request, 'index.html', {'captcha_number': captcha_number})
